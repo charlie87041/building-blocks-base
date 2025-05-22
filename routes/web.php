564275ddcce1316e4e747ecf97dfa6dc067f4cc4
charlie87041 <cgalera87@gmail.com>
2025-05-22 09:38:57 -0400

    adding codex integration

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+//Route::get('/demo', \App\Http\Controllers\Controller::class);
+Route::get('/new_job', \App\Http\Controllers\LeadCreateController::class);
+*/
